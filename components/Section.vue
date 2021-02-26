ab6592b0f6585c8be1b19ad082760d15d0ae04a5
Théo Billardey <theo.billardey@gmail.com>
2021-02-26 12:28:13 +0100

    add type prop in Section

--- components/Section.vue
+++ components/Section.vue
@@ -1,12 +1,22 @@
 <template>
   <section class="section-container">
     <h1>{{ title }}</h1>
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores
-      perspiciatis perferendis illum rerum non facere dolorem, voluptatibus
-      animi at quia reiciendis distinctio voluptas, hic blanditiis dolor nemo id
-      ducimus harum!
-    </p>
+    <div class="shadow radius">
+      <p v-if="type == 1">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores
+        perspiciatis perferendis illum rerum non facere dolorem, voluptatibus
+        animi at quia reiciendis distinctio voluptas, hic blanditiis dolor nemo
+        id ducimus harum!
+      </p>
+      <ul v-else-if="type == 2">
+        <li class="radius" v-for="(repo, i) in repos" :key="i">
+          {{ repo.name }}
+        </li>
+      </ul>
+      <p v-else-if="type == 3">
+        My experience
+      </p>
+    </div>
   </section>
 </template>
 
@@ -17,6 +27,12 @@ export default {
   props: {
     title: {
       type: String
+    },
+    repos: {
+      type: Array
+    },
+    type: {
+      type: Number
     }
   }
 };
@@ -39,5 +55,24 @@ p {
   font-family: "Inter", sans-serif;
   font-size: 1.7rem;
   line-height: 1.6;
+}
+
+div {
+  background: #fff;
+  border: solid var(--white) 1px;
+  padding: 2rem;
+}
+
+ul {
+  display: grid;
+  row-gap: 2rem;
+}
+
+li {
+  list-style: none;
+  background: var(--grey);
+  color: var(--green);
+  padding: 0.4rem 1.6rem;
+  min-height: 10rem;
 }
 </style>
