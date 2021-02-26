14e0dac84f9523ec3eaaa7b35322c895fcccc551
Théo Billardey <theo.billardey@gmail.com>
2021-02-26 10:33:26 +0100

    add Merriweather

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,11 @@ export default {
         rel: "stylesheet",
         href:
           "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400&display=swap"
       }
     ]
   },
