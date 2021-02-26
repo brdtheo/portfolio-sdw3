3d48eb7f29e7ff4aa7cf23b9d5fd211614c1fe28
Théo Billardey <theo.billardey@gmail.com>
2021-02-26 15:18:14 +0100

    add about me section

--- components/Section.vue
+++ components/Section.vue
@@ -1,19 +1,38 @@
 <template>
   <section class="section-container">
     <h1>{{ title }}</h1>
-    <div class="shadow radius">
-      <p v-if="type == 1">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores
-        perspiciatis perferendis illum rerum non facere dolorem, voluptatibus
-        animi at quia reiciendis distinctio voluptas, hic blanditiis dolor nemo
-        id ducimus harum!
+    <div class="about-content shadow radius" v-if="type == 1">
+      <p>
+        Hello everyone 👋
       </p>
-      <ul v-else-if="type == 2">
-        <li class="radius" v-for="(repo, i) in repos" :key="i">
-          {{ repo.name }}
-        </li>
-      </ul>
-      <p v-else-if="type == 3">
+      <p>
+        I'm Théo Billardey and i'm a 20 years old student in web development.
+        I'm currently working on the
+        <a class="badge" :href="links.mim.url" :title="links.mim.title">
+          {{ links.mim.text }}
+        </a>
+        project with Meteor, Vue and Quasar. I'm also working on side projects
+        involving frameworks, libraries that i'm interested in. that you can
+        discover below. You can also take a look at
+        <a class="badge" :href="links.codepen.url" :title="links.codepen.title">
+          {{ links.codepen.text }}
+        </a>
+        since i like to build tiny cool interfaces sometimes.
+      </p>
+      <p>
+        I'm planning to sort of master Vue.js in a few years to build awesome
+        web apps 🤓<br />
+        I'm also interested in UI design since it's very important and it's part
+        of the user experience.
+      </p>
+    </div>
+    <ul class="repos-content shadow radius" v-else-if="type == 2">
+      <li class="radius" v-for="(repo, i) in repos" :key="i">
+        {{ repo.name }}
+      </li>
+    </ul>
+    <div class="experience-content shadow radius" v-else-if="type == 3">
+      <p>
         My experience
       </p>
     </div>
@@ -34,6 +53,23 @@ export default {
     type: {
       type: Number
     }
+  },
+
+  data() {
+    return {
+      links: {
+        mim: {
+          text: "@madeinmenu",
+          url: "https://github.com/madeinmenu",
+          title: "Visit made in menu on GitHub"
+        },
+        codepen: {
+          text: "my codepen profile",
+          url: "https://codepen.io/theo-billardey",
+          title: "brdtheo on codepen"
+        }
+      }
+    };
   }
 };
 </script>
@@ -50,17 +86,27 @@ h1 {
   margin-bottom: 1.8rem;
 }
 
-div,
-p {
+.about-content,
+.repos-content,
+.experience-content {
   font-family: "Inter", sans-serif;
   font-size: 1.7rem;
   line-height: 1.6;
-}
-
-div {
   background: #fff;
   border: solid var(--white) 1px;
   padding: 2rem;
+}
+
+p {
+  margin-bottom: 1.8rem;
+}
+
+p:last-of-type {
+  margin-bottom: 0;
+}
+
+.underline {
+  text-decoration: underline;
 }
 
 ul {
@@ -70,8 +116,7 @@ ul {
 
 li {
   list-style: none;
-  background: var(--grey);
-  color: var(--green);
+  background: var(--white);
   padding: 0.4rem 1.6rem;
   min-height: 10rem;
 }
