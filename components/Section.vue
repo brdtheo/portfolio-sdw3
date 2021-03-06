4f10858b320dc9f000c4e2c7a150e16f6987aa40
Théo Billardey <theo.billardey@gmail.com>
2021-03-06 18:17:20 +0100

    add experience section

--- components/Section.vue
+++ components/Section.vue
@@ -35,11 +35,23 @@
         </a>
       </li>
     </ul>
-    <div class="experience-content shadow radius" v-else-if="type == 3">
-      <p>
-        My experience
-      </p>
-    </div>
+    <ul class="experience-content shadow radius" v-else-if="type == 3">
+      <li class="radius" v-for="(e, i) in experience" :key="i">
+        <div class="main-infos radius">
+          <h2>
+            <span>{{ e.jobTitle }}</span>
+            <span class="experience-date">{{ e.date }}</span>
+          </h2>
+          <p class="experience-location">{{ e.location }}</p>
+          <div class="experience-stack">
+            <span class="badge-sm" v-for="(s, a) in e.stack" :key="a">
+              {{ s }}
+            </span>
+          </div>
+          <p>{{ e.description }}</p>
+        </div>
+      </li>
+    </ul>
     <div class="experience-content shadow radius" v-else-if="type == 4">
       <ul class="skills-grid">
         <li class="skill radius" v-for="(s, i) in skills" :key="i">
@@ -86,6 +98,41 @@ export default {
           title: "brdtheo on codepen"
         }
       },
+
+      experience: [
+        {
+          jobTitle: "Développeur front end",
+          date: "Juin 2020 - Aujourd'hui",
+          description:
+            "Contribution au développement d'une application mobile pour gérer des menus. Gestion des données en temps réel, ajout de librairies comme i18n, chart.js et manipulation d'APIs comme Stripe, Mapbox et OpenWeather.",
+          location: "Made in Menu - Reims, France",
+          stack: [
+            "Meteor",
+            "Vue",
+            "Quasar",
+            "Bootstrap",
+            "jQuery",
+            "Git",
+            "mongoDB"
+          ]
+        },
+        {
+          jobTitle: "Intégrateur web",
+          date: "Janvier 2020",
+          description:
+            "Refonte d'un site internet d'une association. Utilisation de librairie de translate.js (jQuery) et de GitKraken avec un projet sous MVC et mySQL pour le back end.",
+          location: "Il était une fois.. a.s.b.l - Gasperich, Luxembourg",
+          stack: ["HTML", "SASS", "jQuery", "Git"]
+        },
+        {
+          jobTitle: "Technicien de maintenance",
+          date: "2015-2018",
+          description:
+            "Création de prototypes de sites internet, première utilisation d'Ubuntu et découverte du monde de la programmation à travers un stage de 11 semaines réparties sur 3 années. Le service informatique du CHT est composé de plusieurs équipes que j'ai eu la chance d'assister et qui m'ont expliqué les bases de leur métiers.",
+          location: "Centre Hospitalier de Troyes - Troyes, France",
+          stack: ["HTML", "CSS"]
+        }
+      ],
 
       skills: [
         {
@@ -189,12 +236,44 @@ li {
   transition: background-color ease-in 130ms;
 }
 
-li a:hover {
+.repos-content li a:hover {
   background: var(--white-darken);
 }
 
-li a p {
+.repos-content li a p,
+.experience-content li p {
   font-size: 1.4rem;
+}
+
+.experience-content li {
+  background: var(--white);
+  padding: 1.8rem 1.6rem;
+  min-height: 10rem;
+}
+
+.experience-content h2 {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.4rem;
+}
+
+.experience-location {
+  margin-bottom: 0.8rem;
+}
+
+.experience-date {
+  font-size: 1.5rem;
+}
+
+.experience-content .main-infos {
+  display: flex;
+  flex-direction: column;
+}
+
+.experience-stack {
+  display: flex;
+  grid-gap: 0.6rem;
+  margin: 0.6rem 0 0.4rem;
 }
 
 .skills-grid {
