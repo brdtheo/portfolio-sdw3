5db2224c6c9fef8337c471a6e51a4dd3200b5a7d
Théo Billardey <theo.billardey@gmail.com>
2021-03-06 18:32:10 +0100

    26b6ca5

---
+++ _nuxt/static/1615051800/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio-sdw3\u002F_nuxt\u002Fstatic\u002F1615051800",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fportfolio-sdw3\u002F",assetsPath:"\u002Fportfolio-sdw3\u002F_nuxt\u002F",cdnURL:a}}}}(null));
